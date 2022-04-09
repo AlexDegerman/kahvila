97d370e629924c093245fedcedf35e574c454d5e
s2100628-Alex <s2100628@edu.bc.fi>
2022-04-09 05:06:11 +0300

    complete project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'kahvila';
+  tarjoiluOhje: any[] = []
+
+  kahvinMyyty(myyntiData: {poydanNumero:string, myyntiMaara:string}) {
+    this.tarjoiluOhje.push({
+      tyo: 'myyty',
+      poydanNumero: myyntiData.poydanNumero,
+      myyntiMaara: myyntiData.myyntiMaara,
+    });
+  }
+
+  kahvinTarjoilu(tarjoiluData: {poydanNumero:string, myyntiMaara:string}) {
+    this.tarjoiluOhje.push({
+      tyo: 'tarjoiltu',
+      poydanNumero: tarjoiluData.poydanNumero,
+      myyntiMaara: tarjoiluData.myyntiMaara,
+    });
+  }
 }
